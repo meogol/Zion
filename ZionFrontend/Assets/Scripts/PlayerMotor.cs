1449e3b98809f41da7e1f27a268a8b7136921dab
alex <avs99.98@gmail.com>
2022-02-09 16:05:03 +0300

    rebase to master

--- ZionFrontend/Assets/Scripts/PlayerMotor.cs
+++ ZionFrontend/Assets/Scripts/PlayerMotor.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using Unity.Mathematics;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+    [SerializeField]
+    private Camera cam;
+
     private Rigidbody rb;
 
     private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 rotationCamera = Vector3.zero;
 
     private void Start()
     {
@@ -17,10 +23,19 @@ public class PlayerMotor : MonoBehaviour
     {
         velocity = _vel;
     }
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
 
+    public void RotateCam(Vector3 _rotationCam)
+    {
+        rotationCamera = _rotationCam;
+    }
     private void FixedUpdate()
     {
         PerformMove();
+        PerformRotation();
 
     }
 
@@ -28,5 +43,14 @@ public class PlayerMotor : MonoBehaviour
     {
         if (velocity != Vector3.zero)
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-rotationCamera);
+        }
     }
 }

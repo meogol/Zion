7bad012454af5bfb8de5eb75e3f853dc6910cc13
ONEPANTSU <conandet@mail.ru>
2022-02-24 14:26:59 +0300

    Count Of Users

--- Assets/Scripts/Tower/CellSphere.cs
+++ Assets/Scripts/Tower/CellSphere.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CellSphere : MonoBehaviour
 {
-
+    public const int DEFAULT_COUNT_OF_USRES = 666;
+    public int countOfUsers;
     public GameObject obj;
     public GameObject phoneObj;
     public CellComm phone;
@@ -20,7 +21,7 @@ public class CellSphere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        countOfUsers = DEFAULT_COUNT_OF_USRES;
     }
 
     void OnTriggerEnter(Collider other)
@@ -31,6 +32,7 @@ public class CellSphere : MonoBehaviour
             float distance = Mathf.Sqrt(vectorDistance.x * vectorDistance.x + 
                 vectorDistance.y * vectorDistance.y + vectorDistance.z * vectorDistance.z);
             phone.connections.Add(obj.tag, distance);
+            countOfUsers++;
         }
     }
     private void OnTriggerExit(Collider other)
@@ -38,6 +40,7 @@ public class CellSphere : MonoBehaviour
         if (phone.connections.ContainsKey(obj.tag))
         {
             phone.connections.Remove(obj.tag);
+            countOfUsers--;
         }
     }
 

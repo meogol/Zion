fde52c9aa2dfbd25160d16617416e0229b05d0d3
Evgenii Karelin <eugenefolkast@gmail.com>
2022-03-09 16:35:45 +0300

    Bug fix v0.1
    22.02.22
    26.02.22 x2
    24.02.22

--- Assets/Scripts/CameraChanger.cs
+++ Assets/Scripts/CameraChanger.cs
@@ -7,19 +7,53 @@ public class CameraChanger : MonoBehaviour
     private Camera Thecamera;
     public Camera Cam;
 
+    public GameObject exitMenu;
+
+    private bool clicker;
+    private bool cursorVisibility;
+
+    public GameObject player;
+
+    void OffUserControllScripts()
+    {
+        player.GetComponent<PhoneChanger>().enabled = !player.GetComponent<PhoneChanger>().isActiveAndEnabled;
+        
+    }
     void Start()
     {
         Thecamera = GetComponent<Camera>();
         Thecamera = Camera.main;
+
+        cursorVisibility = false;
+        clicker = false;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            OffUserControllScripts();
+
             Thecamera.enabled = ! Thecamera.enabled;
             Cam.enabled = ! Cam.enabled;
+
+            exitMenu.SetActive(!clicker);
+
+
+            Cursor.lockState = CursorLockMode.None;
+            
+            if (cursorVisibility == false)
+            {
+                Cursor.visible = !cursorVisibility;
+            }
+
+            if (!Cam.isActiveAndEnabled)
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
         }
 
+        
     }
 }

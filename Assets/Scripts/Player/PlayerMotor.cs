2cee62c651c746dc79dcc14b6b894d09ba48cfbc
Evgenii Karelin <eugenefolkast@gmail.com>
2022-03-11 16:13:51 +0300

    CameraFix v0.1

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -7,17 +7,29 @@ public class PlayerMotor : MonoBehaviour
     [SerializeField]
     private Camera cam;
 
+
+    [SerializeField]
+    private GameObject playerHead;
+
+
+    [SerializeField]
+    private GameObject playerBody;
+
+
     private Rigidbody rb;
     private float Speed = 1.2f;
     private Vector3 velocity = Vector3.zero;
     private Vector3 rotation = Vector3.zero;
     private Vector3 rotationCamera = Vector3.zero;
     private Vector3 euler;
-
+    private Vector3 eulerBody;
+    private Vector3 eulerHead;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         euler = transform.localEulerAngles;
+        eulerBody = transform.localEulerAngles;
+        eulerHead = transform.localEulerAngles;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -50,9 +62,17 @@ public class PlayerMotor : MonoBehaviour
     void PerformRotation()
     {
         euler.x -= Input.GetAxis("Mouse Y") * Speed;
-        euler.x = Mathf.Clamp(euler.x, -80.0f, 70.0f);
-        euler.y += Input.GetAxis("Mouse X") * Speed;
-        transform.localEulerAngles = euler;
+        euler.x =  Mathf.Clamp(euler.x, -80.0f, 70.0f);
+        cam.transform.localEulerAngles = euler;
+
+
+        eulerHead.z -= Input.GetAxis("Mouse Y") * Speed;
+        eulerHead.z = Mathf.Clamp(euler.x, -80.0f, 70.0f);
+        playerHead.transform.localEulerAngles = eulerHead;
+
+
+        eulerBody.y += Input.GetAxis("Mouse X") * Speed;
+        playerBody.transform.localEulerAngles = eulerBody;
 
         /*rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
         if (cam != null)
